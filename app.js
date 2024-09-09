782ba1103d673db45de2e26d5a6e8c50bdd110fe
owais568 <owaishassan697@gmail.com>
2024-09-09 18:21:33 +0500

    add files

---
+++ app.js
@@ -0,0 +1,219 @@
+// var a=2;
+//    b=1;
+// --a - --b + ++b +b
+// 2   - 1    + 0 +1
+// console.log(2   - 1    + 0 +1)   
+
+document.write("<h1><center>Chapter 5</center></h1>"+"<br>")
+var num1=3;
+var num2=5;
+var res= num1+num2;
+
+document.write("Sum of "+ num1 + " and " + num2 + " is" + res + "<br>");
+
+
+var num1=3;
+var num2=5;
+var res= num1-num2;
+
+document.write("sub of "+ num1 + " and " + num2 + " is" + res + "<br>"); 
+
+
+var num1=3;
+var num2=5;
+var res= num1*num2;
+
+document.write("multiple of "+ num1 + " and " + num2 + " is" + res + "<br>"); 
+
+var num1=3;
+var num2=5;
+var res= num1/num2;
+
+document.write("division of "+ num1 + " and " + num2 + " is" + res + "<br>"); 
+
+document.write("Value after variable declaration is undefined" + "<br>")
+
+document.write("initial value:5" + "<br>")
+var num3=6;
+var num4=1;
+var res= num3+num4;
+document.write("Value after increment is:"+num3 + "<br>")
+var num3=6;
+var num4=7;
+var res= num3+num4;
+document.write("Value after addition is:" +res+ "<br>")
+var num3=6;
+var num4=6;
+var res= num3+num4;
+document.write("Value after decrement is:"+res + "<br>")
+var num5=12;
+var num6=3;
+var res= num5/num6;
+document.write("The remainder is:0" +"<br>"+"<br>")
+var num1=5;
+var num2=600;
+var res=num1*num2;
+document.write("The cost to buy " + num1 + " tickets to a movie is " + res + "PKR" + "<br>"+"<br>")
+var num1=2;
+
+document.write("Table of "+ num1 +"<br>")
+document.write("2x1= "+ num1*1 +"<br>")
+document.write("2x2= "+ num1*2 +"<br>")
+document.write("2x3= "+ num1*3 +"<br<b>>")
+document.write("2x4= "+ num1*4 +"<br>")
+document.write("2x5= "+ num1*5 +"<br>")
+document.write("2x6= "+ num1*6 +"<br>")
+document.write("2x7= "+ num1*7 +"<br>")
+document.write("2x8= "+ num1*8 +"<br>")
+document.write("2x9= "+ num1*9 +"<br>")
+document.write("2x10= "+ num1*10 +"<br>"+"<br>")
+var num1=77
+var res=(num1-32)*5/9
+document.write(res+"C"+" is "+77+"F"+"<br>")
+var num1=21.11111111
+var res=(num1*9/5) +32
+document.write(res+"F"+" is "+num1+"F"+"<br>"+"<br>")
+var num1=650
+var num2=100
+var num3=3
+var num4=7
+var num5=100
+var res=(num1*num3)+(num2*num4)+num5
+document.write("Price of item 1 is "+ num1 +"<br>")
+document.write("Quantity of item 1 is "+ num3 +"<br>")
+document.write("Price of item 2 is "+ num2 +"<br>")
+document.write("Quantity of item 2 is "+ num4 +"<br>")
+document.write("Shipping Charges "+ num5 +"<br>"+"<br>")
+document.write("Total cost of your order is "+ res +"<br>"+"<br>")
+document.write("<b>MARKS SHEET</b>" +"<br>")
+var num1=980
+var num2=804
+var res=(num2*100)/num1
+document.write("Total marks: "+ num1 +"<br>")
+document.write("Marks obtained: "+ num2 +"<br>")
+document.write(" Percentage: "+ res +"<br>"+"<br>")
+document.write("<b>Currency in PKR</b>"+"<br>")
+var num1=10
+var num2=104.8
+var num3=25
+var num4=28
+var res=(num1*num2)+(num3*num4)
+document.write(" Total Currency in PKR: "+ res +"<br>"+"<br>")
+var num1=2
+document.write(num1+ 5 +"<br>")
+document.write(num1* 10 +"<br>")
+document.write(num1/2 +"<br>" +"<br>")
+document.write("<b>Age Calculator</b>"+"<br>")
+var num1=2016
+var num2=1992
+var res=num1-num2
+document.write("Current Year: "+ num1 +"<br>")
+document.write("Birth Year: "+ num2 +"<br>")
+document.write("Your Age: "+ res +"<br>")
+document.write("<b>The Geometrizer</b>"+"<br>"+"<br>")
+var num1=20
+document.write("Radius of a circle: "+ num1 +"<br>")
+document.write("The circumference is: "+ 2*3.14*num1 +"<br>")
+document.write("Radius of a circle: "+ 3.14*(num1*num1)+"<br>"+"<br>")
+document.write("<b>The Lifetime Supply Calculator</b>"+"<br>");
+var num1=15
+var num2=65
+var num3=3
+document.write("Favourite Snack:"+"chocolate chip"+"<br>")
+document.write("Current age:"+ num1 +"<br>")
+document.write("Estimated Maximun Age:"+ num2 +"<br>")
+document.write("Amount of snacks per day:"+ num3 +"<br>")
+document.write("You will need" + (num2-num1)*num3 + " chocolate chip to last you will the rip old age of " + num2 +"<br>"+"<br>")
+document.write("<h1><center>Chapter 9 start</center></h1>"+"<br>"+"<br>")
+var num1=10;
+var num2=1
+document.write("Result:"+"<br>")
+document.write("The value of a is: "+ num1 +"<br>")
+document.write("....................."+"<br>"+"<br>")
+document.write("The value of ++a is:"+ (num1+1) +"<br>")
+document.write("Now the value of a is:"+ (num1+1) +"<br>"+"<br>")
+document.write("The value of a++ is:"+ (num1+1) +"<br>")
+document.write("Now the value of a is:"+ (num1+2) +"<br>"+"<br>")
+document.write("The value of -a is:"+ (num1+1) +"<br>")
+document.write("Now the value of a is:"+ (num1+1) +"<br>"+"<br>")
+document.write("The value of a-- is:"+ (num1+1) +"<br>")
+document.write("Now the value of a is:"+ num1 +"<br>"+"<br>")
+
+var a=2;
+var b=1;
+var res1=--a - --b
+var res2=--a - --b + ++b
+var res3=--a - --b + ++b + b--
+document.write("--a;"+ (a-1)+"<br>")
+document.write("--a - --b;"+ res1 +"<br>")   
+document.write("--a - --b + ++b;"+ res2 +"<br>")
+document.write("--a - --b + ++b + b--;"+ res3 +"<br>"+"<br>")
+var num1=100
+var num2=54
+var num3=48
+var res1=num1*3
+var res2=num2+num2+num3
+var res3=(res2)/3
+document.write("<b>Subjects</b> "+" <b>Total Marks</b> "+" <b>Obtained Marks</b> "+"<b>Percentage</b>"+"<br>")
+document.write("English "+ " 100 " + " 54 " +" 54% "+"<br>")
+document.write("Urdu "+ "100 " +" 54 " +"54% "+"<br>")
+document.write("Math "+ " 100" +" 48 " +"48% "+"<br>")
+document.write("Total marks:"+ res1 +"<br>")
+document.write("Total obtained marks:"+ res2 +"<br>")
+document.write(" overall percentage:"+ res3 +"<br>")
+document.write("<h1><center>EXERCISE EXPRESSIONS</center></h1>"+"<br>")
+var x=100
+var res=100-1
+document.write("answer:"+res+"<br>")
+var x=50
+var y=x++
+document.write("answer:"+ y +"<br>")
+var y=50
+var z=-y
+document.write("answer:"+z+"<br>")
+var a=2
+var res=--a
+document.write("answer:"+res+"<br>")
+var a=2
+var b=a+1
+document.write("answer:"+b+"<br>")
+// var a=2;
+// var res=a++;
+// alert(res);
+var calculatedNum=2+(2*6)
+var res=calculatedNum
+document.write("answer:"+ res +"<br>")
+var calculatedNum=(2+2)*6
+var res=calculatedNum
+document.write("answer:"+ res +"<br>")
+var calculatedNum=(2+2)*(4+2)
+var res=calculatedNum
+document.write("answer:"+ res +"<br>")
+var calculatedNum=((2+2)+4)+2
+var res=calculatedNum
+document.write("answer:"+ res +"<br>")
+var a=36
+var res=2+2*4+10+a
+document.write("answer:"+ res +"<br>")
+var a=0
+var res=2+2*4+10+a
+document.write("answer:"+ res +"<br>")
+var a=4
+var res=(2+2*4+10)/a
+document.write("answer:"+ res +"<br>")
+var num="2"+"2"
+document.write("answer:"+ num +"<br>")
+// alert("Hellow,"+"Dolly")
+// alert("33"+3)
+// alert("Pakistan Zindabad")
+var result="The answer is:"+42;
+document.write(result+"<br>")
+var num1="virat:"+18
+var num2="rohit;"+10
+var res=num1+num2
+document.write(res+"<br>")
+var userName=prompt("Enter first name")
+var userCountry=prompt("Country name","China")
+var yourName=prompt("Enter your name","jhon");
+var lastName=prompt("enter your last name","jhon white");
+var phoneNumber=prompt("enter your number","+92..........")
